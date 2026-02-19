4743e47394cafb58d595bab3e7a8772eda3feff5
hibiki848 <hibiki848@users.noreply.github.com>
2026-02-19 13:17:58 +0900

    Railway対応：PORT環境変数で起動するよう修正

--- server.js
+++ server.js
@@ -872,5 +872,5 @@ app.use((err, req, res, next) => {
 // ---------- Listen ----------
 const PORT = Number(process.env.PORT || 3000);
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
